e596e078ea385738e3b795c4947a01cf0eb8abe2
pa596bu500 <pa596bu500@gmail.com>
2024-04-30 01:20:49 +0800

    misc update

--- lib/up_helper/model/user_provider.dart
+++ lib/up_helper/model/user_provider.dart
@@ -1,8 +1,13 @@
+import 'package:buff_helper/pkg_buff_helper.dart';
 import 'package:flutter/material.dart';
 import 'mdl_user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class UserProvider extends ChangeNotifier {
   Evs2User? _currentUser;
+  User? firebaseUser;
+
+  UserProvider({this.firebaseUser});
 
   Evs2User? get currentUser => _currentUser;
 
