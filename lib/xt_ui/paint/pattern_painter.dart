02231212a2063b949296fa61ae218703d8c747ad
pa596 <oliver.lu@pa.com.sg>
2024-05-03 12:23:41 +0800

    custom painter

---
+++ lib/xt_ui/paint/pattern_painter.dart
@@ -0,0 +1,46 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class PatternPainter extends CustomPainter {
+  const PatternPainter({
+    Key? key,
+    this.pattern = 'solid',
+    this.color = Colors.black,
+    this.spacing = 10,
+    this.width,
+    this.height,
+  });
+  final String pattern;
+  final Color color;
+  final double spacing;
+  final double? width;
+  final double? height;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = color // Set the color of the pattern
+      ..style = PaintingStyle.stroke; // Set the painting style to stroke
+
+    // Define the spacing between the lines
+    double spacing = this.spacing;
+
+    if (pattern == 'diagonal') {
+      // Draw diagonal lines across the canvas
+      for (double i = 0;
+          i < (width ?? size.width) + (height ?? size.height);
+          i += spacing * sqrt(2)) {
+        canvas.drawLine(
+          Offset(0, i),
+          Offset((width ?? size.width), i - (width ?? size.width)),
+          paint,
+        );
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
