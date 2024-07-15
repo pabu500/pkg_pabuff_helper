b5d691d4b467b6d83ae7e0c66e90715bc9ad3cff
pa596 <oliver.lu@pa.com.sg>
2024-07-15 14:24:38 +0800

    color update

--- lib/xt_ui/style/evs2_colors.dart
+++ lib/xt_ui/style/evs2_colors.dart
@@ -10,12 +10,16 @@ const Color evs2SUTDwhite = Color(0xffffffff);
 
 final Color commitColor = Colors.amber.shade900.withOpacity(0.95);
 
-final Color pagCallToAction = Colors.amber.shade700.withOpacity(0.95);
-
-const Color pagNeo2 = Color.fromARGB(255, 160, 220, 235);
-const Color pagNeo = Color.fromARGB(255, 150, 210, 220);
+const Color pagNeo = Color.fromARGB(255, 160, 220, 235);
+const Color pagNeo2 = Color.fromARGB(255, 120, 190, 210);
 const Color sgGreenLight = Color.fromARGB(255, 0, 203, 203);
-const Color spGreen1 = Color(0xff00abb5);
-const Color spGreen2 = Color(0xff2184a1);
-const Color spGreen3 = Color(0xff2a5267);
-const Color spBlue1 = Color(0xff38567c);
+const Color spGreen1 = Color.fromARGB(255, 0, 171, 181);
+const Color spGreen2 = Color.fromARGB(255, 33, 132, 161);
+const Color spGreen3 = Color.fromARGB(255, 42, 82, 103);
+const Color spBlue1 = Color.fromARGB(255, 56, 86, 124);
+
+const Color pag1 = pagNeo2;
+const Color pag2 = spBlue1;
+const Color pag3 = evs2NTUyellow;
+const Color pagCallToActionFace = spGreen3;
+const Color? pagCallToActionText = null;
