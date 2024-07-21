87c1e0437df70358a60e96afc6ca64a8557205eb
pa596bu500 <pa596bu500@gmail.com>
2024-07-22 02:56:09 +0800

    misc update

--- lib/xt_ui/wdgt/info/get_error_text_prompt.dart
+++ lib/xt_ui/wdgt/info/get_error_text_prompt.dart
@@ -4,33 +4,38 @@ Widget getErrorTextPrompt(
     {required BuildContext context,
     required String errorText,
     String? title,
-    double? margin}) {
+    double? margin,
+    Color? textColor,
+    Color? bgColor,
+    Color? borderColor}) {
   return Padding(
     padding: EdgeInsets.symmetric(vertical: margin ?? 5.0),
     child: Container(
-      padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 8.0),
+      padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 13.0),
       decoration: BoxDecoration(
+        color: bgColor,
         border: Border.all(
-          color: Theme.of(context).colorScheme.error,
+          color: borderColor ?? Theme.of(context).colorScheme.error,
           width: 1,
         ),
         borderRadius: BorderRadius.circular(5),
       ),
       child: Column(
         mainAxisSize: MainAxisSize.min,
+        // crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           if (title != null)
             Text(
               title,
               style: TextStyle(
-                color: Theme.of(context).colorScheme.error,
+                color: textColor ?? Theme.of(context).colorScheme.error,
                 fontWeight: FontWeight.bold,
               ),
             ),
           Text(
             errorText,
             style: TextStyle(
-              color: Theme.of(context).colorScheme.error,
+              color: textColor ?? Theme.of(context).colorScheme.error,
             ),
           ),
         ],
