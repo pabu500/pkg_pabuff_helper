6ec15f189185e658b3ed772fe523fa6639a72619
pa596bu500 <pa596bu500@gmail.com>
2024-12-23 00:50:58 +0800

    misc

---
+++ lib/pag_helper/comm/comm_app.dart
@@ -0,0 +1,93 @@
+import 'dart:convert';
+
+import 'package:buff_helper/pag_helper/comm/pag_be_api_base.dart';
+import 'package:buff_helper/pag_helper/model/mdl_pag_app_config.dart';
+import 'package:buff_helper/pag_helper/model/mdl_pag_user.dart';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'package:buff_helper/up_helper/up_helper.dart';
+
+import '../../../../pag_console/lib/app_config.dart';
+
+Future<dynamic> getVersion2(String appName) async {
+  String projectScope = pagAppConfig.activePortalPagProjectScopeList[0].name;
+
+  try {
+    String url =
+        '${PagUrlController(null, pagAppConfig).getUrl(PagSvcType.oresvc2, PagUrlBase.eptGetVerion)}/$appName/$projectScope';
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final respJson = jsonDecode(response.body);
+      return respJson['version'] ?? '';
+    } else {
+      throw Exception('Failed to load version');
+    }
+  } catch (err) {
+    if (kDebugMode) {
+      print(err);
+    }
+    rethrow;
+  }
+}
+
+Future<dynamic> getOreVersion2(MdlPagUser? loggedInUser) async {
+  try {
+    final response = await http.get(
+      Uri.parse(PagUrlController(loggedInUser, pagAppConfig)
+          .getUrl(PagSvcType.oresvc2, PagUrlBase.eptOreHello)),
+    );
+
+    if (response.statusCode == 200) {
+      // final respJson = jsonDecode(response.body);
+      String respJson = response.body;
+      return respJson;
+    } else {
+      throw Exception('ORE handshake failed');
+    }
+  } catch (err) {
+    return err.toString();
+  }
+}
+
+Future<dynamic> getOaxLink(
+  MdlPagAppConfig appConfig,
+  MdlPagUser? loggedInUser,
+  Map<String, dynamic> queryMap,
+  SvcClaim svcClaim,
+) async {
+  svcClaim.svcName = PagSvcType.oresvc2.name;
+  svcClaim.endpoint = PagUrlBase.eptGetOaxLink;
+
+  String svcToken = '';
+  // try {
+  //   svcToken = await svcGate(svcClaim /*, queryByUser*/);
+  // } catch (err) {
+  //   throw Exception(err);
+  // }
+
+  final response = await http.post(
+    Uri.parse(PagUrlController(loggedInUser, pagAppConfig)
+        .getUrl(PagSvcType.oresvc2, svcClaim.endpoint!)),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer $svcToken',
+    },
+    body: jsonEncode(SvcQuery(svcClaim, queryMap).toJson()),
+  );
+
+  try {
+    if (response.statusCode == 200) {
+      final respJson = jsonDecode(response.body);
+      if (respJson['oax_link'] == null) {
+        throw Exception('Failed to get oax link');
+      }
+      return respJson['oax_link'];
+    } else {
+      throw Exception('Failed to get oax link');
+    }
+  } catch (err) {
+    // return err.toString();
+    rethrow;
+  }
+}
