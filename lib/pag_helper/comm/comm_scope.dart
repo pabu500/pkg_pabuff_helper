2ec9d116b6f633b37af97327a0c542062dd84933
pa596 <oliver.lu@pa.com.sg>
2024-12-30 18:10:22 +0800

    misc

---
+++ lib/pag_helper/comm/comm_scope.dart
@@ -0,0 +1,133 @@
+import 'dart:convert';
+
+import 'package:buff_helper/pag_helper/comm/pag_be_api_base.dart';
+import 'package:buff_helper/pag_helper/model/acl/mdl_pag_svc_claim.dart';
+import 'package:buff_helper/pag_helper/model/mdl_pag_app_config.dart';
+import 'package:buff_helper/pag_helper/model/mdl_pag_user.dart';
+import 'package:buff_helper/pag_helper/model/mdl_svc_query.dart';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+Future<dynamic> getUserRoleScopeList(
+  MdlPagAppConfig appConfig,
+  MdlPagUser? loggedInUser,
+  Map<String, dynamic> queryMap,
+  MdlPagSvcClaim svcClaim,
+) async {
+  svcClaim.svcName = PagSvcType.oresvc2.name;
+  svcClaim.endpoint = PagUrlBase.eptGetUserRoleScopeList;
+
+  String svcToken = '';
+  // try {
+  //   svcToken = await svcGate(svcClaim /*, queryByUser*/);
+  // } catch (err) {
+  //   throw Exception(err);
+  // }
+  try {
+    final response = await http.post(
+      Uri.parse(PagUrlController(loggedInUser, appConfig)
+          .getUrl(PagSvcType.oresvc2, svcClaim.endpoint!)),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer $svcToken',
+      },
+      body: jsonEncode(MdlPagSvcQuery(svcClaim, queryMap).toJson()),
+    );
+
+    if (response.statusCode == 200) {
+      final respJson = jsonDecode(response.body);
+      if (respJson['error'] != null) {
+        throw Exception(respJson['error']);
+      }
+      if (respJson['data'] == null) {
+        throw Exception('Failed to get user role list');
+      }
+
+      var data = respJson['data'];
+      if (data['user_role_scope_list'] == null) {
+        throw Exception('Failed to get user role scope list');
+      }
+
+      final scopeListJson = data['user_role_scope_list'];
+
+      List<Map<String, dynamic>> scopeInfoList = [];
+      if (scopeListJson != null) {
+        for (var item in scopeListJson) {
+          scopeInfoList.add(item);
+        }
+      }
+
+      return {
+        'user_role_scope_list': scopeInfoList,
+      };
+    } else if (response.statusCode == 403) {
+      throw Exception("You are not authorized to perform this operation");
+    } else {
+      throw Exception(jsonDecode(response.body)['error']);
+    }
+  } catch (e) {
+    if (kDebugMode) {
+      print('getUserRoleScopeList error: $e');
+    }
+    rethrow;
+  }
+}
+
+Future<dynamic> getLocationGroupLocationList(
+  MdlPagAppConfig appConfig,
+  MdlPagUser? loggedInUser,
+  Map<String, dynamic> queryMap,
+  MdlPagSvcClaim svcClaim,
+) async {
+  svcClaim.svcName = PagSvcType.oresvc2.name;
+  svcClaim.endpoint = PagUrlBase.eptGetLocationGroupLocationList;
+
+  String svcToken = '';
+  // try {
+  //   svcToken = await svcGate(svcClaim /*, queryByUser*/);
+  // } catch (err) {
+  //   throw Exception(err);
+  // }
+
+  final response = await http.post(
+    Uri.parse(PagUrlController(loggedInUser, appConfig)
+        .getUrl(PagSvcType.oresvc2, svcClaim.endpoint!)),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer $svcToken',
+    },
+    body: jsonEncode(MdlPagSvcQuery(svcClaim, queryMap).toJson()),
+  );
+
+  if (response.statusCode == 200) {
+    final respJson = jsonDecode(response.body);
+    if (respJson['error'] != null) {
+      throw Exception(respJson['error']);
+    }
+    if (respJson['data'] == null) {
+      throw Exception('Failed to get location list');
+    }
+
+    var data = respJson['data'];
+    if (data['location_group_location_list'] == null) {
+      throw Exception('Failed to get location list');
+    }
+
+    final locationListJson = data['location_group_location_list'];
+
+    List<Map<String, dynamic>> locationInfoList = [];
+    if (locationListJson != null) {
+      for (var item in locationListJson) {
+        locationInfoList.add(item);
+      }
+    }
+
+    return {
+      'location_group_location_list': locationInfoList,
+    };
+  } else if (response.statusCode == 403) {
+    throw Exception("You are not authorized to perform this operation");
+  } else {
+    throw Exception(jsonDecode(response.body)['error']);
+  }
+}
