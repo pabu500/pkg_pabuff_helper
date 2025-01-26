b89d9292dc570c3ae135da830986576bf5fdb41c
pa596bu500 <pa596bu500@gmail.com>
2025-01-26 22:22:19 +0800

    layout customize

--- lib/pag_helper/model/app/mdl_page_config.dart
+++ lib/pag_helper/model/app/mdl_page_config.dart
@@ -2,7 +2,8 @@ import 'package:buff_helper/pag_helper/def/def_panel.dart';
 
 import 'mdl_page_config_item.dart';
 
-const int defaultPageGridWidth = 128;
+const int defaultPageGridWidthGrid = 128;
+const int defaultPageGridWidthList = 85;
 
 const int defaultTopStatCount = 6;
 const int defaultMainPanelRowCount = 3;
@@ -11,7 +12,7 @@ const int defaultTopStatGridWidth = 21;
 const int defaultTopStatGridHeight = 11;
 
 const int defaultMainPanelGridWidth = 63;
-const int defaultMainPanelGridHeight = 16;
+const int defaultMainPanelGridHeight = 28;
 
 // const int geoPaneGridWidth = 63;
 // const int geoPaneGridHeight = 34;
@@ -22,7 +23,7 @@ const int defaultMainPanelGridHeight = 16;
 class MdlPagPageConfig {
   String name;
 
-  int pageGridWidth = defaultPageGridWidth;
+  // int pageGridWidth = defaultPageGridWidth;
   int topStatCount = defaultTopStatCount;
   int mainPanelRowCount = defaultMainPanelRowCount;
 
@@ -30,11 +31,17 @@ class MdlPagPageConfig {
 
   MdlPagPageConfig({
     required this.name,
-    this.pageGridWidth = defaultPageGridWidth,
+    // this.pageGridWidth = defaultPageGridWidth,
     this.topStatCount = defaultTopStatCount,
     this.mainPanelRowCount = defaultMainPanelRowCount,
     this.panelList,
   });
+
+  void updateSelectedPageLayout(String layoutKey) {
+    for (MdlPagPageConfigItem panelConfig in panelList!) {
+      panelConfig.selectedLayoutKey = layoutKey;
+    }
+  }
 
   factory MdlPagPageConfig.fromJson(Map<String, dynamic> json) {
     if (json['name'] == null) {
@@ -43,10 +50,10 @@ class MdlPagPageConfig {
 
     String pageName = json['name'];
 
-    dynamic pageGridWidth = json['page_grid_width'] ?? defaultPageGridWidth;
-    if (pageGridWidth is String) {
-      pageGridWidth = int.tryParse(pageGridWidth);
-    }
+    // dynamic pageGridWidth = json['page_grid_width'] ?? defaultPageGridWidth;
+    // if (pageGridWidth is String) {
+    //   pageGridWidth = int.tryParse(pageGridWidth);
+    // }
     dynamic topStatCount = json['top_stat_count'] ?? defaultTopStatCount;
     if (topStatCount is String) {
       topStatCount = int.tryParse(topStatCount);
@@ -58,6 +65,8 @@ class MdlPagPageConfig {
     }
 
     List<MdlPagPageConfigItem> panelConfigList = [];
+
+    // plotting topleft x and y for each panel
     if (json['panel_config_list'] != null) {
       int indexTopStat = 0;
       int indexMainPanel = 0;
@@ -86,71 +95,180 @@ class MdlPagPageConfig {
           continue;
         }
 
-        panelConfig.gridWidth =
-            panelConfig.gridWidth ?? defaultTopStatGridWidth;
-        panelConfig.gridHeight =
-            panelConfig.gridHeight ?? defaultTopStatGridHeight;
+        for (Map<String, dynamic> layout in panelConfig.layoutList) {
+          dynamic pageGridWidth =
+              json['page_grid_width'] ?? defaultPageGridWidthGrid;
+          if (layout['layout_key'] == 'list') {
+            pageGridWidth = json['page_grid_width'] ?? defaultPageGridWidthList;
+          }
+          if (pageGridWidth is String) {
+            pageGridWidth = int.tryParse(pageGridWidth);
+          }
 
-        assert(panelConfig.colIndex != null);
-        assert(panelConfig.rowIndex != null);
+          layout['grid_width'] =
+              layout['grid_width'] ?? defaultTopStatGridWidth;
+          layout['grid_height'] =
+              layout['grid_height'] ?? defaultTopStatGridHeight;
 
-        panelConfig.gridTopLeftY = 0;
-        panelConfig.gridTopLeftX = (panelConfig.colIndex! *
-                (panelConfig.gridWidth ?? defaultTopStatGridWidth))
-            .toInt();
+          layout['row_index'] = panelConfig.rowIndex;
+          layout['col_index'] = panelConfig.colIndex;
+          assert(layout['col_index'] != null);
+          assert(layout['row_index'] != null);
 
-        topStatGridHeight = panelConfig.gridHeight!;
-        totalTopStatWidth += panelConfig.gridWidth!;
+          layout['grid_top'] = 0;
+          layout['grid_left'] = (layout['col_index'] *
+                  (layout['grid_width'] ?? defaultTopStatGridWidth))
+              .toInt();
+
+          topStatGridHeight = layout['grid_height'];
+          totalTopStatWidth += layout['grid_width'] as int;
+        }
+        // panelConfig.gridWidth = panelConfig.gridWidth ?? defaultTopStatGridWidth;
+        // panelConfig.gridHeight = panelConfig.gridHeight ?? defaultTopStatGridHeight;
+
+        // assert(panelConfig.colIndex != null);
+        // assert(panelConfig.rowIndex != null);
+
+        // panelConfig.gridTopLeftY = 0;
+        // panelConfig.gridTopLeftX = (panelConfig.colIndex! * (panelConfig.gridWidth ?? defaultTopStatGridWidth)).toInt();
+
+        // topStatGridHeight = panelConfig.gridHeight!;
+        // totalTopStatWidth += panelConfig.gridWidth!;
       }
 
       // if total top stat width exceed page grid width, update page grid width
-      if (totalTopStatWidth > pageGridWidth) {
-        pageGridWidth = totalTopStatWidth;
-      }
+      // if (totalTopStatWidth > pageGridWidth) {
+      //   pageGridWidth = totalTopStatWidth;
+      // }
 
       // calculate grid position for main_panel
-      int currentRowIndex = 0;
-      if (indexTopStat > 0) {
-        currentRowIndex = 1;
-      }
-      int currentColIndex = 0;
-      int currentMainGridWidth = 0;
-      int currentMainGridTopLeftY = 0;
-      if (indexTopStat > 0) {
-        currentMainGridTopLeftY = topStatGridHeight;
-      }
+      // int currentRowIndex = 0;
+      // if (indexTopStat > 0) {
+      //   currentRowIndex = 1;
+      // }
+      // int currentColIndex = 0;
+      // int currentMainGridWidth = 0;
+      // int currentMainGridTopLeftY = 0;
+      // if (indexTopStat > 0) {
+      //   currentMainGridTopLeftY = topStatGridHeight;
+      // }
+
+      // get list of layout keys for main panel
+      List<String> layoutKeys = [];
       for (MdlPagPageConfigItem panelConfig in panelConfigList) {
         if (panelConfig.type == PagPanelType.topStat) {
           continue;
         }
 
-        panelConfig.gridWidth =
-            panelConfig.gridWidth ?? defaultMainPanelGridWidth;
-        panelConfig.gridHeight =
-            panelConfig.gridHeight ?? defaultMainPanelGridHeight;
+        for (Map<String, dynamic> layout in panelConfig.layoutList) {
+          if (!layoutKeys.contains(layout['layout_key'])) {
+            layoutKeys.add(layout['layout_key']);
+          }
+        }
+      }
 
-        assert(panelConfig.gridWidth != null);
-        assert(panelConfig.gridHeight != null);
-
-        // move row index and column index to next row if exceed page grid width
-        currentMainGridWidth += panelConfig.gridWidth!;
-        bool widthExceed = currentMainGridWidth > pageGridWidth;
-        bool mainColExceed = currentColIndex > mainPanelColCount;
-        if (widthExceed || mainColExceed) {
-          currentRowIndex++;
-          currentColIndex = 0;
-          currentMainGridWidth = 0;
-
-          panelConfig.rowIndex = currentRowIndex;
-          panelConfig.colIndex = currentColIndex;
-
-          currentMainGridTopLeftY += panelConfig.gridHeight!;
+      for (String layoutKey in layoutKeys) {
+        int currentRowIndex = 0;
+        if (indexTopStat > 0) {
+          currentRowIndex = 1;
+        }
+        int currentColIndex = 0;
+        int currentMainGridWidth = 0;
+        int currentMainGridTopLeftY = 0;
+        if (indexTopStat > 0) {
+          currentMainGridTopLeftY = topStatGridHeight;
         }
 
-        panelConfig.gridTopLeftY = currentMainGridTopLeftY;
-        panelConfig.gridTopLeftX = currentColIndex * panelConfig.gridWidth!;
+        for (MdlPagPageConfigItem panelConfig in panelConfigList) {
+          if (panelConfig.type == PagPanelType.topStat) {
+            continue;
+          }
 
-        currentColIndex++;
+          // find layout with layout key
+          Map<String, dynamic> layout = {};
+          for (Map<String, dynamic> layoutMap in panelConfig.layoutList) {
+            if (layoutMap['layout_key'] == layoutKey) {
+              layout = layoutMap;
+              break;
+            }
+          }
+
+          dynamic pageGridWidth =
+              json['page_grid_width'] ?? defaultPageGridWidthGrid;
+          if (layoutKey == 'list') {
+            pageGridWidth = json['page_grid_width'] ?? defaultPageGridWidthList;
+          }
+          if (pageGridWidth is String) {
+            pageGridWidth = int.tryParse(pageGridWidth);
+          }
+
+          // if total top stat width exceed page grid width, update page grid width
+          if (totalTopStatWidth > pageGridWidth) {
+            pageGridWidth = totalTopStatWidth;
+          }
+
+          layout['grid_width'] =
+              layout['grid_width'] ?? defaultMainPanelGridWidth;
+          layout['grid_height'] =
+              layout['grid_height'] ?? defaultMainPanelGridHeight;
+
+          layout['row_index'] = panelConfig.rowIndex;
+          layout['col_index'] = panelConfig.colIndex;
+          assert(layout['col_index'] != null);
+          assert(layout['row_index'] != null);
+
+          layout['grid_top'] = currentMainGridTopLeftY;
+          layout['grid_left'] = (layout['col_index'] *
+                  (layout['grid_width'] ?? defaultMainPanelGridWidth))
+              .toInt();
+
+          // move row index and column index to next row if exceed page grid width
+          currentMainGridWidth += layout['grid_width'] as int;
+          bool widthExceed = currentMainGridWidth > pageGridWidth;
+          bool mainColExceed = currentColIndex > mainPanelColCount;
+          if (widthExceed || mainColExceed) {
+            currentRowIndex++;
+            currentColIndex = 0;
+            currentMainGridWidth = layout['grid_width'];
+
+            layout['row_index'] = currentRowIndex;
+            layout['col_index'] = currentColIndex;
+
+            currentMainGridTopLeftY += layout['grid_height'] as int;
+          }
+
+          layout['grid_top'] = currentMainGridTopLeftY;
+          layout['grid_left'] = currentColIndex *
+              (layout['grid_width'] ?? defaultMainPanelGridWidth);
+
+          currentColIndex++;
+        }
+
+        // panelConfig.gridWidth = panelConfig.gridWidth ?? defaultMainPanelGridWidth;
+        // panelConfig.gridHeight = panelConfig.gridHeight ?? defaultMainPanelGridHeight;
+
+        // assert(panelConfig.gridWidth != null);
+        // assert(panelConfig.gridHeight != null);
+
+        // move row index and column index to next row if exceed page grid width
+        // currentMainGridWidth += panelConfig.gridWidth!;
+        // bool widthExceed = currentMainGridWidth > pageGridWidth;
+        // bool mainColExceed = currentColIndex > mainPanelColCount;
+        // if (widthExceed || mainColExceed) {
+        //   currentRowIndex++;
+        //   currentColIndex = 0;
+        //   currentMainGridWidth = 0;
+
+        //   panelConfig.rowIndex = currentRowIndex;
+        //   panelConfig.colIndex = currentColIndex;
+
+        //   currentMainGridTopLeftY += panelConfig.gridHeight!;
+        // }
+
+        // panelConfig.gridTopLeftY = currentMainGridTopLeftY;
+        // panelConfig.gridTopLeftX = currentColIndex * panelConfig.gridWidth!;
+
+        // currentColIndex++;
       }
     }
 
--- lib/pag_helper/model/app/mdl_page_config_item.dart
+++ lib/pag_helper/model/app/mdl_page_config_item.dart
@@ -1,4 +1,5 @@
 import 'package:buff_helper/pag_helper/def/def_panel.dart';
+import 'package:flutter/foundation.dart';
 
 class MdlPagPageConfigItem {
   String name;
@@ -6,11 +7,13 @@ class MdlPagPageConfigItem {
   PagPanelType type;
   int? rowIndex;
   int? colIndex;
-  int? gridTopLeftY;
-  int? gridTopLeftX;
-  int? gridWidth;
-  int? gridHeight;
+  // int? gridTopLeftY;
+  // int? gridTopLeftX;
+  // int? gridWidth;
+  // int? gridHeight;
   String? decorationStr;
+  List<Map<String, dynamic>> layoutList;
+  String selectedLayoutKey = '';
 
   MdlPagPageConfigItem({
     required this.name,
@@ -18,12 +21,29 @@ class MdlPagPageConfigItem {
     required this.type,
     this.rowIndex,
     this.colIndex,
-    this.gridTopLeftY,
-    this.gridTopLeftX,
-    this.gridWidth,
-    this.gridHeight,
+    // this.gridTopLeftY,
+    // this.gridTopLeftX,
+    // this.gridWidth,
+    // this.gridHeight,
     this.decorationStr,
+    this.layoutList = const [],
   });
+
+  Map<String, dynamic> getSelectedLayout() {
+    if (selectedLayoutKey.isEmpty) {
+      return layoutList.first;
+    }
+    for (Map<String, dynamic> layout in layoutList) {
+      if (layout['layout_key'] == selectedLayoutKey) {
+        return layout;
+      }
+    }
+    // throw Exception('Invalid selectedLayoutKey: $selectedLayoutKey');
+    if (kDebugMode) {
+      print('Invalid selectedLayoutKey: $selectedLayoutKey');
+    }
+    return layoutList.first;
+  }
 
   factory MdlPagPageConfigItem.fromJson(Map<String, dynamic> json) {
     if (json['name'] == null) {
@@ -44,22 +64,70 @@ class MdlPagPageConfigItem {
           'MdlPagPageConfigItem.fromJson: Invalid type: ${json['type']}');
     }
 
-    dynamic gridTop = json['grid_top'];
-    if (gridTop is String) {
-      gridTop = int.tryParse(gridTop);
+    List<Map<String, dynamic>> layoutList = [];
+
+    if (type != PagPanelType.topStat) {
+      if (json['layout_list'] == null) {
+        throw Exception('Invalid layout_list');
+      }
     }
-    dynamic gridLeft = json['grid_left'];
-    if (gridLeft is String) {
-      gridLeft = int.tryParse(gridLeft);
+    for (Map<String, dynamic> layoutMap in json['layout_list'] ?? []) {
+      // Map<String, dynamic> layout = {};
+
+      String? layoutKey = layoutMap['layout_key'];
+      if (layoutKey == null) {
+        throw Exception('Invalid layout_key');
+      }
+
+      dynamic gridTop = layoutMap['grid_top'];
+      if (gridTop is String) {
+        gridTop = int.tryParse(gridTop);
+      }
+      dynamic gridLeft = layoutMap['grid_left'];
+      if (gridLeft is String) {
+        gridLeft = int.tryParse(gridLeft);
+      }
+      dynamic gridW = layoutMap['grid_width'];
+      if (gridW is String) {
+        gridW = int.tryParse(gridW);
+      }
+      dynamic gridH = layoutMap['grid_height'];
+      if (gridH is String) {
+        gridH = int.tryParse(gridH);
+      }
+
+      layoutList.add({
+        'layout_key': layoutKey,
+        'grid_top': gridTop,
+        'grid_left': gridLeft,
+        'grid_width': gridW,
+        'grid_height': gridH,
+      });
     }
-    dynamic gridW = json['grid_width'];
-    if (gridW is String) {
-      gridW = int.tryParse(gridW);
+
+    if (type != PagPanelType.topStat) {
+      assert(layoutList.isNotEmpty);
     }
-    dynamic gridH = json['grid_height'];
-    if (gridH is String) {
-      gridH = int.tryParse(gridH);
+    if (type == PagPanelType.topStat) {
+      layoutList.add({});
     }
+
+    // dynamic gridTop = json['grid_top'];
+    // if (gridTop is String) {
+    //   gridTop = int.tryParse(gridTop);
+    // }
+    // dynamic gridLeft = json['grid_left'];
+    // if (gridLeft is String) {
+    //   gridLeft = int.tryParse(gridLeft);
+    // }
+    // dynamic gridW = json['grid_width'];
+    // if (gridW is String) {
+    //   gridW = int.tryParse(gridW);
+    // }
+    // dynamic gridH = json['grid_height'];
+    // if (gridH is String) {
+    //   gridH = int.tryParse(gridH);
+    // }
 
     return MdlPagPageConfigItem(
       name: json['name'],
@@ -67,10 +135,11 @@ class MdlPagPageConfigItem {
       type: type,
       rowIndex: json['row_index'],
       colIndex: json['col_index'],
-      gridTopLeftY: gridTop,
-      gridTopLeftX: gridLeft,
-      gridWidth: gridW,
-      gridHeight: gridH,
+      // gridTopLeftY: gridTop,
+      // gridTopLeftX: gridLeft,
+      // gridWidth: gridW,
+      // gridHeight: gridH,
+      layoutList: layoutList,
       decorationStr: json['decoration_str'],
     );
   }
