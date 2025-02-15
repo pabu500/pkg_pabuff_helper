181af9509466a79bf51663c40fb2a1f959d5375a
pa596bu500 <pa596bu500@gmail.com>
2025-02-15 19:22:22 +0800

    pag scope update

--- lib/pag_helper/model/scope/mdl_pag_scope.dart
+++ lib/pag_helper/model/scope/mdl_pag_scope.dart
@@ -1,27 +1,56 @@
 class MdlPagScope {
   String projectName;
   String projectId;
+  String? projectLabel;
   String? siteGroupName;
   String? siteGroupId;
+  String? siteGroupLabel;
   String? siteName;
   String? siteId;
+  String? siteLabel;
   String? buildingName;
   String? buildingId;
+  String? buildingLabel;
   String? locationGroupName;
   String? locationGroupId;
+  String? locationGroupLabel;
 
   MdlPagScope({
     required this.projectName,
     required this.projectId,
+    this.projectLabel,
     this.siteGroupName,
     this.siteGroupId,
+    this.siteGroupLabel,
     this.siteName,
     this.siteId,
+    this.siteLabel,
     this.buildingName,
     this.buildingId,
+    this.buildingLabel,
     this.locationGroupName,
     this.locationGroupId,
+    this.locationGroupLabel,
   });
+
+  String getLeafScopeLabel() {
+    if (locationGroupLabel != null) {
+      return locationGroupLabel!;
+    }
+    if (buildingLabel != null) {
+      return buildingLabel!;
+    }
+    if (siteLabel != null) {
+      return siteLabel!;
+    }
+    if (siteGroupLabel != null) {
+      return siteGroupLabel!;
+    }
+    if (projectLabel != null) {
+      return projectLabel!;
+    }
+    return projectName;
+  }
 
   factory MdlPagScope.fromJson(Map<String, dynamic> json) {
     if (json.isEmpty) {
@@ -36,14 +65,19 @@ class MdlPagScope {
     return MdlPagScope(
       projectName: json['project_name'],
       projectId: json['project_id'],
+      projectLabel: json['project_label'],
       siteGroupName: json['site_group_name'],
       siteGroupId: json['site_group_id'],
+      siteGroupLabel: json['site_group_label'],
       siteName: json['site_name'],
       siteId: json['site_id'],
+      siteLabel: json['site_label'],
       buildingName: json['building_name'],
       buildingId: json['building_id'],
+      buildingLabel: json['building_label'],
       locationGroupName: json['location_group_name'],
       locationGroupId: json['location_group_id'],
+      locationGroupLabel: json['location_group_label'],
     );
   }
 
@@ -51,14 +85,19 @@ class MdlPagScope {
     return {
       'project_name': projectName,
       'project_id': projectId,
+      'project_label': projectLabel,
       'site_group_name': siteGroupName,
       'site_group_id': siteGroupId,
+      'site_group_label': siteGroupLabel,
       'site_name': siteName,
       'site_id': siteId,
+      'site_label': siteLabel,
       'building_name': buildingName,
       'building_id': buildingId,
+      'building_label': buildingLabel,
       'location_group_name': locationGroupName,
       'location_group_id': locationGroupId,
+      'location_group_label': locationGroupLabel,
     };
   }
 }
