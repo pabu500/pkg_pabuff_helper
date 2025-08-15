35b75b4eca382f99e3bb8b114ec57e4ab0422986
pa596 <oliver.lu@pa.com.sg>
2025-08-15 16:29:19 +0800

    misc

--- lib/pag_helper/model/list/mdl_list_controller.dart
+++ lib/pag_helper/model/list/mdl_list_controller.dart
@@ -162,7 +162,7 @@ class MdlPagListController /*extends ChangeNotifier*/ {
     String itemTypeStr = 'unknown_item_type';
     if (itemType is PagDeviceCat) {
       // itemTypeStr = getPagDeviceTypeStr(itemType);
-      itemTypeStr = itemType.name; // use the enum's name
+      itemTypeStr = (itemType as PagDeviceCat).name; // use the enum's name
       // switch (itemType) {
       //   case PagDeviceCat.meter:
       //     itemTypeStr = 'meter';
